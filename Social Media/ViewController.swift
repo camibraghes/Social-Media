7bf0a487434eb05ee1b404e137e34094a9852c55
Camelia Braghes <cameliabraghes@gmail.com>
2023-01-09 14:39:12 +0200

    Create a flags list and replace the assets

--- Social Media/ViewController.swift
+++ Social Media/ViewController.swift
@@ -24,13 +24,14 @@ class ViewController: UITableViewController {
         }
         print(flags)
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return flags.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
+        cell.textLabel?.text = flags[indexPath.row]
         return cell
     }
 }
